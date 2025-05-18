63dd48fd6adf9a9f4e00012f14b1c1b1aa842ea0
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-05-18 22:05:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -30,12 +30,12 @@ const teams = [
     { name: "ASAL", logo: "./asal.jpeg", mp: 7, w: 3, d: 0, l: 4, gf: 16, ga: 19, gd: -3, pts: 9 },
     { name: "BIKASH", logo: "bikash.jpeg", mp: 6, w: 2, d: 0, l: 4, gf: 15, ga: 19, gd: -4, pts: 6 },
     { name: "PARU DAI", logo: "parudai.jpeg", mp: 5, w: 3, d: 0, l: 2, gf: 15, ga: 12, gd: 3, pts: 9 },
-    { name: "AASHISH", logo: "aashish.jpeg", mp: 5, w: 3, d: 0, l: 2, gf: 15, ga: 9, gd: 6, pts: 9 },
+    { name: "AASHISH", logo: "aashish.jpeg", mp: 6, w: 4, d: 0, l: 2, gf: 19, ga: 10, gd: 9, pts: 12 },
     { name: "ANJAN", logo: "anjan.jpeg", mp: 6, w: 3, d: 0, l: 3, gf: 14, ga: 13, gd: 1, pts: 9 },
     { name: "DEV", logo: "dev.jpeg", mp: 5, w: 5, d: 0, l: 0, gf: 13, ga: 5, gd: 8, pts: 15 },
     { name: "ANISH", logo: "anish.jpeg", mp: 7, w: 2, d: 0, l: 5, gf: 15, ga: 26, gd: -11, pts: 6 },
-    { name: "RAAJ", logo: "raaj.jpeg", mp: 5, w: 2, d: 0, l: 3, gf: 12, ga: 13, gd: -2, pts: 6 },
-    { name: "BINAYA", logo: "binaya.jpeg", mp: 4, w: 2, d: 0, l: 2, gf: 11, ga: 9, gd: 2, pts: 6 }
+    { name: "RAAJ", logo: "raaj.jpeg", mp: 6, w: 2, d: 0, l: 4, gf: 13, ga: 19, gd: -6, pts: 6 },
+    { name: "BINAYA", logo: "binaya.jpeg", mp: 6, w: 3, d: 0, l: 3, gf: 18, ga: 14, gd: 4, pts: 9 }
 ];
 
 // Function to sort and rank teams
