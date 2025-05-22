6ad5c9bd4ababd4fae6a79c48050d9970436970e
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-05-22 16:26:23 +0545

    Update script.js

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ const teams = [
     { name: "ASAL", logo: "./asal.jpeg", mp: 11, w: 6, d: 0, l: 5, gf: 28, ga: 27, gd: 1, pts: 18 },
     { name: "BIKASH", logo: "bikash.jpeg", mp: 11, w: 4, d: 0, l: 7, gf: 25, ga: 32, gd: -7, pts: 12 },
     { name: "PARU DAI", logo: "parudai.jpeg", mp: 11, w: 5, d: 1, l: 5, gf: 26, ga: 25, gd: 1, pts: 16 },
-    { name: "AASHISH", logo: "aashish.jpeg", mp: 11, w: 8, d: 1, l: 2, gf: 32, ga: 14, gd: 18, pts: 25 },
+    { name: "AASHISH", logo: "aashish.jpeg", mp: 12, w: 9, d: 1, l: 2, gf: 35, ga: 15, gd: 20, pts: 28 },
     { name: "ANJAN", logo: "anjan.jpeg", mp: 11, w: 7, d: 0, l: 4, gf: 31, ga: 23, gd: 8, pts: 21 },
     { name: "DEV", logo: "dev.jpeg", mp: 10, w: 5, d: 2, l: 3, gf: 18, ga: 14, gd: 4, pts: 17 },
-    { name: "ANISH", logo: "anish.jpeg", mp: 12, w: 6, d: 0, l: 6, gf: 32, ga: 36, gd: -4, pts: 18 },
+    { name: "ANISH", logo: "anish.jpeg", mp: 13, w: 6, d: 0, l: 7, gf: 33, ga: 39, gd: -6, pts: 18 },
     { name: "RAAJ", logo: "raaj.jpeg", mp: 10, w: 2, d: 0, l: 8, gf: 17, ga: 35, gd: -18, pts: 6 },
     { name: "BINAYA", logo: "binaya.jpeg", mp: 11, w: 4, d: 0, l: 7, gf: 29, ga: 31, gd: -2, pts: 12 }
 ];
