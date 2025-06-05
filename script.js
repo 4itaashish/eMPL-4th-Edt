476dcca84279786f8fc11961e25b4814c1e1a0e2
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-06-05 22:40:15 +0545

    Update script.js

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ const season3GroupA = [
 const season3GroupB = [
   { name: "AASHISH", mp: 7, w: 4, d: 2, l: 1, gf: 24, ga: 11, gd: 13, pts: 14 },
   { name: "KUSHAL", mp: 5, w: 3, d: 1, l: 1, gf: 20, ga: 5, gd: 15, pts: 10 },
-  { name: "SAJINA", mp: 5, w: 2, d: 0, l: 3, gf: 8, ga: 22, gd: -14, pts: 6 },
-  { name: "DEV", mp: 5, w: 3, d: 1, l: 1, gf: 14, ga: 9, gd: 5, pts: 10 },
+  { name: "SAJINA", mp: 6, w: 2, d: 0, l: 4, gf: 9, ga: 24, gd: -15, pts: 6 },
+  { name: "DEV", mp: 6, w: 4, d: 1, l: 1, gf: 16, ga: 10, gd: 6, pts: 13 },
   { name: "BINAYA", mp: 8, w: 1, d: 0, l: 7, gf: 5, ga: 24, gd: -19, pts: 3 },
 ]
 
